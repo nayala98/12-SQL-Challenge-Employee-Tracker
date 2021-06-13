0eeb338de28dff5c5cfc5344f848a2a825a98179
nayala98 <nathaniel.ayala20@gmail.com>
2021-06-13 18:53:56 -0500

    added updateEmployeeRole function

--- index.js
+++ index.js
@@ -327,5 +327,63 @@ function removeEmployee() {
 };
 
 function updateEmployeeRole() {
+    var empChoice = [];
+    connection.query("SELECT id, first_name, last_name FROM employees", function(err, resEmp) {
+        if (err) throw err;
+        for (var i = 0; i < resEmp.length; i++) {
+            var empList = resEmp[i].first_name + " " + resEmp[i].last_name;
+            empChoice.push(empList);
+        };
 
+    var roleChoice = [];
+    connection.query("SELECT * FROM role", function(err, resRole) {
+        if (err) throw err;
+        for (var i = 0; i < resRole.length; i++) {
+        var roleList = resRole[i].title;
+        roleChoice.push(roleList);
+        };
+
+    inquirer
+    .prompt([
+    {
+        name: "employee_id",
+        type: "rawlist",
+        message: "Select the employee you would like to update:",
+        choices: empChoice
+    },
+    {
+        name: "role_id",
+        type: "rawlist",
+        message: "Select employee's new role:",
+        choices: roleChoice
+    }
+    ])
+    .then(function(answer) {
+
+    var chosenEmp;
+            for (var i = 0; i < resEmp.length; i++) {
+            if (resEmp[i].first_name || resRole[i].last_name === answer.employee_id) {
+                chosenEmp = resEmp[i];
+            }
+        };
+
+    var chosenRole;
+        for (var i = 0; i < resRole.length; i++) {
+            if (resRole[i].title === answer.role_id) {
+            chosenRole = resRole[i];
+            }
+        };
+        connection.query(
+            "UPDATE employees SET role_id = ? WHERE id = ?",
+
+            [chosenRole.id, chosenEmp.id],
+            function(err) {
+            if (err) throw err;
+            console.log("Employee new role successfully updated!");
+            startApp();
+            }
+        );
+    })
+    })
+    })
 };
